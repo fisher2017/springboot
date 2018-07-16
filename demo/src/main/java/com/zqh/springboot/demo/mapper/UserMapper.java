65b67816386c3b11fa961f79b1bb41f079e85093
zhangqihui <qhzhang@tplhk.cntaiping.com>
2018-07-16 14:45:43 +0800

    add

---
+++ demo/src/main/java/com/zqh/springboot/demo/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.zqh.springboot.demo.mapper;
+
+import com.zqh.springboot.demo.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
